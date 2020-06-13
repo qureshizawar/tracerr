5695f27c9ae1c13d17db5736675f022e1c3fdeb8
qureshizawar <qureshizawar@gmail.com>
2020-06-13 02:49:52 +0100

    tweaking build process and dep automation

---
+++ init_proto.sh
@@ -0,0 +1,3 @@
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-cpp-3.12.3.tar.gz
+tar -xzf protobuf-cpp-3.12.3.tar.gz
+cd protobuf-3.12.3 && ./configure && make && make check && make install && ldconfig
