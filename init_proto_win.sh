e95ef9c7f60c2716e8b01e9af43687ce365ae007
Zawar Qureshi <qureshizawar@gmail.com>
2020-06-14 16:51:07 +0100

    make diff proto builds

--- init_proto_win.sh
+++ init_proto_win.sh
@@ -1,3 +1,3 @@
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-cpp-3.12.3.tar.gz
 tar -xzf protobuf-cpp-3.12.3.tar.gz
-cd protobuf-3.12.3 && ./configure --disable-dependency-tracking && make && make check && sudo make install && sudo ldconfig
+cd protobuf-3.12.3 && ./configure --disable-dependency-tracking && make && make check && make install # && ldconfig
