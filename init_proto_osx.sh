fb5474376844754bd559d538480785f5215025f5
Zawar Qureshi <31623134+qureshizawar@users.noreply.github.com>
2020-06-15 01:47:45 +0100

    Update init_proto_osx.sh

--- init_proto_osx.sh
+++ init_proto_osx.sh
@@ -1,4 +1,4 @@
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
 tar -xzf protobuf-cpp-3.6.1.tar.gz
-cd protobuf-3.6.1 && ./configure && make && make install && ldconfig
+cd protobuf-3.6.1 && ./configure && make && make install #&& ldconfig
 #brew install protobuf@3.6 && echo 'export PATH="/usr/local/opt/protobuf@3.6/bin:$PATH"' >> /Users/travis/.bash_profile && export LDFLAGS="-L/usr/local/opt/protobuf@3.6/lib" && export CPPFLAGS="-I/usr/local/opt/protobuf@3.6/include" && export PKG_CONFIG_PATH="/usr/local/opt/protobuf@3.6/lib/pkgconfig"
