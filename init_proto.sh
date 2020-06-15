9e158f15ca765228f66c61f18fa88e9ba7c3bc74
Zawar Qureshi <31623134+qureshizawar@users.noreply.github.com>
2020-06-15 01:00:49 +0100

    Update init_proto.sh

--- init_proto.sh
+++ init_proto.sh
@@ -1,3 +1,3 @@
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
 tar -xzf protobuf-cpp-3.6.1.tar.gz
-cd protobuf-3.6.1 && ./configure && make && make check && make install && ldconfig
+cd protobuf-3.6.1 && ./configure && make && make install && ldconfig
